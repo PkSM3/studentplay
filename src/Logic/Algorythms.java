74c7b26550fa1bd2f88d4632c7a42fdb84eef483
PkSM3 <pokesam3@gmail.com>
2013-03-20 11:37:53 -0300

    doing monitorMarkedExams

--- src/Logic/Algorythms.java
+++ src/Logic/Algorythms.java
@@ -19,10 +19,11 @@ import logicetoile.Main;
 
 public class Algorythms {
 
-/****************************************************************/
-/*         < ALGORYTHMS TO SELECT EXAM MARKERS >
- * 
- */
+    /**
+     * *************************************************************
+     */
+    /*         < ALGORYTHMS TO SELECT EXAM MARKERS >
+     */
     private MySQL inst;
     static final int numberOfMarkersPerQuestion = 1;
 
@@ -79,28 +80,31 @@ public class Algorythms {
         return sum / markers_list.size();
     }
 
-    public void print(String content){
+    public void print(String content) {
         System.out.println(content);
     }
-    
+
     /*  Algorithm for single marker selection using Roulette wheel  */
     public Marker ASMS_RW(ArrayList<String> markers_list, int topic, int direction) {
 
         float RW_range = calculateOverallTempRep(direction, markers_list, topic);
-        
+
         for (String mlist : markers_list) {
             for (Marker m : Main.markersAndReputation) {
                 if (mlist.equals(m.getMail())) {
                     for (ReputationTopic rep : m.getReputation_topic()) {
                         if (rep.getTopic_id() == topic) {
-                            if(direction==0) RW_range += rep.getTemporalReputation();
-                            else RW_range += (1-rep.getTemporalReputation());
+                            if (direction == 0) {
+                                RW_range += rep.getTemporalReputation();
+                            } else {
+                                RW_range += (1 - rep.getTemporalReputation());
+                            }
                         }
                     }
                 }
             }
         }
-        
+
         Random r = new Random();
         float roulette_position = r.nextFloat() * (0.00001f + RW_range);
         float reputation_sum = 0;
@@ -112,13 +116,12 @@ public class Algorythms {
                     if (mlist.equals(m.getMail())) {
                         for (ReputationTopic rep : m.getReputation_topic()) {
                             if (rep.getTopic_id() == topic) {
-                                if(direction==0) {
+                                if (direction == 0) {
                                     reputation_sum += rep.getTemporalReputation();
-                                }   
-                                else {
-                                    reputation_sum += (1-rep.getTemporalReputation());
+                                } else {
+                                    reputation_sum += (1 - rep.getTemporalReputation());
                                 }
-                                
+
                                 if (reputation_sum >= roulette_position) {
                                     selected_marker = m;
                                     rep.setTemporalReputation(rep.getTemporalReputation() - 0.15f);
@@ -171,7 +174,7 @@ public class Algorythms {
                     //SubmittedExams == ExpectedSubmittedExams
                     //AND submittedExams > 1
                     ArrayList<Question_Markers> marksXquests = ASXMS(sub);
-                    System.out.println(marksXquests.get(1).getQuestion_markers_list().get(0));
+                    //System.out.println("lala: "+marksXquests.get(1).getQuestion_markers_list().get(0));
                     if (Main.exam_markers[sub.getSubmittedExam_id()] == null) {
                         Main.exam_markers[sub.getSubmittedExam_id()] = new Exam_Markers();
                         Main.exam_markers[sub.getSubmittedExam_id()].setMarkersPerQuestion(marksXquests);
@@ -182,15 +185,53 @@ public class Algorythms {
             }
         }
 
-    }    
-/*         </ ALGORYTHMS TO SELECT EXAM MARKERS >
-/****************************************************************/
+    }
+    /*
+     /*         </ ALGORYTHMS TO SELECT EXAM MARKERS >
+     /****************************************************************/
+
+    
+    
+    /**
+     * *************************************************************
+     */
+    /*         < ALGORYTHMS TO CALCULATE THE SCORE OF EXAMS >
+     */
     
     
     
     
-/****************************************************************/
-/*         < ALGORYTHMS TO CALCULATE THE SCORE OF EXAMS >
- */
-    
+    /*Algorithm for monitoring marked exams:*/
+    public void monitorMarkedExams()
+            throws ClassNotFoundException,
+            InstantiationException,
+            IllegalAccessException,
+            SQLException {
+        
+        /* obtain submittedExams */
+        Set set = Main.exams.entrySet();
+        Iterator examID = set.iterator();
+        while (examID.hasNext()) {//while (i -> Submitted Exams) {
+            Map.Entry iterator = (Map.Entry) examID.next();
+            int i = (int) iterator.getKey();
+            print("exam-0" + i);
+            for (SubmittedExam e : Main.submttExams[i].getSubmExams()) {
+                if(e.getReviewd()==0){
+                    print("\tsubmExamID: " + e.getSubmittedExam_id() + " - "+ e.getMail() + " - reviewed: " + e.getReviewd());
+                }
+            }
+        }
+        /* obtain submittedExams */
+        
+    }
+
+    /*Algorithm to calculate student_exam_score: ASCRX (student, exam_id) :*/
+    public void ASCRX() {
+    }
+
+    /*Algorithm to calculate student_answer_score: ASCA (answer_id):*/
+    public void ASCRA() {
+    }
+    /*         </ ALGORYTHMS TO CALCULATE THE SCORE OF EXAMS >
+     /****************************************************************/
 }
--- src/logicetoile/Main.java
+++ src/logicetoile/Main.java
@@ -17,33 +17,17 @@ public class Main {
     //public static int [] modules = new int[10];
 
     public static HashMap exams = new HashMap();/* { test.id , module.id} */
-
     public static Exam[] submttExams = new Exam[10];
     public static Module[] modules = new Module[10];
     public static ArrayList<Marker> markersAndReputation = new ArrayList<Marker>();
     public static Exam_Markers[] exam_markers = new Exam_Markers[10];
 
-    public static void main(String[] args) {
+    public static void createExamMarkers() {
         try {
-
             Algorythms inst = new Algorythms();
             inst.monitorNewSubmttExams();
-            MySQL close = new MySQL();
-            close.conn.close();
-
-//            System.out.println();
-//            for (Marker m : markersAndReputation) {
-//                System.out.print(m.getMail());
-//                System.out.print(": {");
-//                for(ReputationTopic rep : m.getReputation_topic()){
-//                    System.out.print(
-//                             rep.getTopic_id()+", "
-//                            +rep.getReputation()+", "
-//                            +rep.getTemporalReputation());
-//                }
-//                System.out.print("}");
-//            }
-//            System.out.println();
+            MySQL mysql = new MySQL();
+            mysql.conn.close();
             for (int j = 0; j < Main.exam_markers.length; j++) {
                 if (Main.exam_markers[j] != null) {
                     System.out.println("ID Submitted Exam: " + j);
@@ -51,9 +35,8 @@ public class Main {
                     for (int k = 0; k < Main.exam_markers[j].getMarkersPerQuestion().size(); k++) {
                         System.out.println("Question ID " + Main.exam_markers[j].getMarkersPerQuestion().get(k).getQuestion_id());
                         for (int o = 0; o < Main.exam_markers[j].getMarkersPerQuestion().get(k).getQuestion_markers_list().size(); o++) {
-                            System.out.println(
-                                    Main.exam_markers[j].getMarkersPerQuestion().get(k).getQuestion_markers_list().get(o)
-                                    );
+                            System.out.println("lilu: "
+                                    + Main.exam_markers[j].getMarkersPerQuestion().get(k).getQuestion_markers_list().get(o));
                         }
                     }
                 } else {
@@ -61,8 +44,6 @@ public class Main {
                 }
                 System.out.println();
             }
-
-
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
@@ -72,5 +53,31 @@ public class Main {
         } catch (SQLException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    private static void calculateExamsScore() {
+        try {
+            
+            
+            Algorythms inst = new Algorythms();
+            inst.monitorMarkedExams();
+            
+            
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        createExamMarkers();
+        System.out.println("\n*********************************************\n");
+        calculateExamsScore();
     }
 }
